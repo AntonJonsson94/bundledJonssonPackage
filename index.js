51a435a6f6294e5d77a7b3598b04e0a52c1f5f03
Anton Jonsson <94antjon@skola.boras.se>
2023-06-05 13:27:15 +0200

    added error handling

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ printInput();
 function printInput() {
     body.innerHTML = "";
     const input = document.createElement("input");
-    input.setAttribute("type", "number");
+    input.setAttribute("placeholder", "How old are you?");
     const button = document.createElement("button");
     button.innerText = "Send";
 
@@ -22,11 +22,33 @@ function printInput() {
 function story(age) {
     body.innerHTML = "";
     const message = document.createElement("p");
-    if (age >= 18) {
+
+    if (isNaN(age)) {
+        message.innerText = "Please write a number";
+        const retryButton = document.createElement("button");
+        retryButton.innerText = "Retry";
+        body.appendChild(retryButton);
+        retryButton.addEventListener("click", () => {
+            printInput();
+        });
+    } else if (age >= 18) {
         message.innerText = `${printAnswer(age)}. Take a ride around town!`;
+        const backButton = document.createElement("button");
+        backButton.innerText = "Back";
+        backButton.addEventListener("click", () => {
+            printInput();
+        });
+        body.appendChild(backButton);
     } else {
         message.innerText = `${printAnswer(age)} GO HOME!`;
+        const backButton = document.createElement("button");
+        backButton.innerText = "Back";
+        backButton.addEventListener("click", () => {
+            printInput();
+        });
+        body.appendChild(backButton);
     }
+
     body.appendChild(message);
 }
 
